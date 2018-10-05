62e787a11c984fb9ec950c21ef99ee60866880e0
Phil Moir <pgmoir@gmail.com>
2018-10-05 15:16:38 +0100

    Push both package.json files. Update core text on main page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I was changed again!
+            This is the new version!
           </p>
           <a
             className="App-link"
